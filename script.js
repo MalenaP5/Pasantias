35090a267d538c97c9b1541e3bcead4590d44b07
Ezekiel122EVM <eaflores@colegiosolari.edu.ar>
2024-10-02 00:10:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Ejemplo básico de interacción
-document.querySelector('button').addEventListener('click', function() {
-    const searchValue = document.getElementById('searchInput').value;
-    alert('Buscando recetas para: ' + searchValue);
-});
